03be29a7fc6f5d6946ebaf36ba13640cde60fab9
Rigoberto Rios <rigobertoriosh@hotmail.com>
2019-07-23 11:10:28 -0500

    se a gregaron algunos comentaris en app

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-
-var url = window.location.href;
+var url = window.location.href; //optiene toda la referencia de la url en la que la app se encuentra
 var swLocation = '/twittor/sw.js';
 
 
-if ( navigator.serviceWorker ) {
+if (navigator.serviceWorker) {
 
 
-    if ( url.includes('localhost') ) {
+    if (url.includes('localhost')) { //para modificar la ubicación del SW, dependiendo de si esta en producción o en el localhost
         swLocation = '/sw.js';
     }
 
 
-    navigator.serviceWorker.register( swLocation );
+    navigator.serviceWorker.register(swLocation);
 }
 
 
@@ -20,18 +19,18 @@ if ( navigator.serviceWorker ) {
 
 // Referencias de jQuery
 
-var titulo      = $('#titulo');
-var nuevoBtn    = $('#nuevo-btn');
-var salirBtn    = $('#salir-btn');
+var titulo = $('#titulo');
+var nuevoBtn = $('#nuevo-btn');
+var salirBtn = $('#salir-btn');
 var cancelarBtn = $('#cancel-btn');
-var postBtn     = $('#post-btn');
-var avatarSel   = $('#seleccion');
-var timeline    = $('#timeline');
+var postBtn = $('#post-btn');
+var avatarSel = $('#seleccion');
+var timeline = $('#timeline');
 
-var modal       = $('#modal');
+var modal = $('#modal');
 var modalAvatar = $('#modal-avatar');
-var avatarBtns  = $('.seleccion-avatar');
-var txtMensaje  = $('#txtMensaje');
+var avatarBtns = $('.seleccion-avatar');
+var txtMensaje = $('#txtMensaje');
 
 // El usuario, contiene el ID del héroe seleccionado
 var usuario;
@@ -43,7 +42,7 @@ var usuario;
 
 function crearMensajeHTML(mensaje, personaje) {
 
-    var content =`
+    var content = `
     <li class="animated fadeIn fast">
         <div class="avatar">
             <img src="img/avatars/${ personaje }.jpg">
@@ -68,9 +67,9 @@ function crearMensajeHTML(mensaje, personaje) {
 
 
 // Globals
-function logIn( ingreso ) {
+function logIn(ingreso) {
 
-    if ( ingreso ) {
+    if (ingreso) {
         nuevoBtn.removeClass('oculto');
         salirBtn.removeClass('oculto');
         timeline.removeClass('oculto');
@@ -83,7 +82,7 @@ function logIn( ingreso ) {
         avatarSel.removeClass('oculto');
 
         titulo.text('Seleccione Personaje');
-    
+
     }
 
 }
@@ -111,18 +110,18 @@ salirBtn.on('click', function() {
 nuevoBtn.on('click', function() {
 
     modal.removeClass('oculto');
-    modal.animate({ 
+    modal.animate({
         marginTop: '-=1000px',
         opacity: 1
-    }, 200 );
+    }, 200);
 
 });
 
 // Boton de cancelar mensaje
 cancelarBtn.on('click', function() {
-   modal.animate({ 
-       marginTop: '+=1000px',
-       opacity: 0
+    modal.animate({
+        marginTop: '+=1000px',
+        opacity: 0
     }, 200, function() {
         modal.addClass('oculto');
         txtMensaje.val('');
@@ -133,11 +132,11 @@ cancelarBtn.on('click', function() {
 postBtn.on('click', function() {
 
     var mensaje = txtMensaje.val();
-    if ( mensaje.length === 0 ) {
+    if (mensaje.length === 0) {
         cancelarBtn.click();
         return;
     }
 
-    crearMensajeHTML( mensaje, usuario );
+    crearMensajeHTML(mensaje, usuario);
 
 });
